9bfbdb62bbaa9b9a3de72ed002acd45e7de1f3da
Arman Akhmetov <eon704@gmail.com>
2025-02-01 03:50:22 -0500

    Scenes

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+  [SerializeField] private Image _foreground;
+    
+  public void FinishGame()
+  {
+    this.StartCoroutine(this.LoadMainMenu());
+  }
+    
+  private IEnumerator Start()
+  {
+    yield return this.ForegroundFadeOut();
+  }
+
+  private IEnumerator LoadMainMenu()
+  {
+    yield return this.ForegroundFadeIn();
+    yield return new WaitForSeconds(0.5f);
+    AsyncOperation loadSceneAsync = SceneManager.LoadSceneAsync("MainMenu");
+    yield return new WaitUntil(() => loadSceneAsync!.isDone);
+  }
+    
+  private IEnumerator ForegroundFadeIn()
+  {
+    this._foreground.color = new Color(0, 0, 0, 0);
+    this._foreground.gameObject.SetActive(true);
+        
+    Tween tween = this._foreground
+                      .DOFade(1, 1)
+                      .SetEase(Ease.InCubic);
+
+    yield return tween.WaitForCompletion();
+  }
+    
+  private IEnumerator ForegroundFadeOut()
+  {
+    this._foreground.color = Color.black;
+    this._foreground.gameObject.SetActive(true);
+        
+    Tween tween = this._foreground
+                      .DOFade(0, 1)
+                      .SetEase(Ease.OutCubic);
+
+    yield return new WaitForSeconds(0.5f);
+    yield return tween.WaitForCompletion();
+    this._foreground.gameObject.SetActive(false);
+  }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Image _foreground;
+    
+    public void StartGame()
+    {
+        this.StartCoroutine(this.LoadGame());
+    }
+    
+    private IEnumerator Start()
+    {
+        yield return this.ForegroundFadeOut();
+    }
+
+    private IEnumerator LoadGame()
+    {
+        yield return this.ForegroundFadeIn();
+        yield return new WaitForSeconds(0.5f);
+        AsyncOperation loadSceneAsync = SceneManager.LoadSceneAsync("Game");
+        yield return new WaitUntil(() => loadSceneAsync!.isDone);
+    }
+    
+    private IEnumerator ForegroundFadeIn()
+    {
+        this._foreground.color = new Color(0, 0, 0, 0);
+        this._foreground.gameObject.SetActive(true);
+        
+        Tween tween = this._foreground
+                          .DOFade(1, 1)
+                          .SetEase(Ease.InCubic);
+
+        yield return tween.WaitForCompletion();
+    }
+    
+    private IEnumerator ForegroundFadeOut()
+    {
+        this._foreground.color = Color.black;
+        this._foreground.gameObject.SetActive(true);
+        
+        Tween tween = this._foreground
+                          .DOFade(0, 1)
+                          .SetEase(Ease.OutCubic);
+
+        yield return new WaitForSeconds(0.5f);
+        yield return tween.WaitForCompletion();
+        this._foreground.gameObject.SetActive(false);
+    }
+}
