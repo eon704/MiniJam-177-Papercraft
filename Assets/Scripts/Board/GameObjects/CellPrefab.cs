f4c11219134a5f0c6171bdf5bf7966263197962e
Arman Akhmetov <eon704@gmail.com>
2025-02-11 00:37:43 -0500

    Minor update for Mobile compatibility

--- Assets/Scripts/Board/GameObjects/CellPrefab.cs
+++ Assets/Scripts/Board/GameObjects/CellPrefab.cs
@@ -1,7 +1,8 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CellPrefab : MonoBehaviour
+public class CellPrefab : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private SpriteRenderer fill;
     [SerializeField] private GameObject fire;
@@ -21,10 +22,10 @@ public class CellPrefab : MonoBehaviour
     
     private Sequence pulseSequence;
 
-    public void Initialize(Cell cellData, Player player)
+    public void Initialize(Cell cellData, Player newPlayer)
     {
         this.Cell = cellData;
-        this.player = player;
+        this.player = newPlayer;
 
         this.start.SetActive(this.Cell.Terrain == Cell.TerrainType.Start);
         this.end.SetActive(this.Cell.Terrain == Cell.TerrainType.End);
@@ -43,25 +44,25 @@ public class CellPrefab : MonoBehaviour
         
         return pulseSequence;
     }
-
+    
     public void ResetPulse()
     {
         pulseSequence?.Kill();
         this.fill.color = this.defaultColor;
     }
 
-    private void OnMouseUp()
-    {
-        this.player.Move(this);
-    }
-
-    private void OnMouseEnter()
+    public void OnPointerEnter(PointerEventData eventData)
     {
         this.fill.color = this.highlightColor;
     }
-
-    private void OnMouseExit()
+    public void OnPointerExit(PointerEventData eventData)
     {
         this.fill.color = this.defaultColor;
+    }
+    public void OnPointerDown(PointerEventData eventData) {}
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.player.Move(this);
     }
 }
