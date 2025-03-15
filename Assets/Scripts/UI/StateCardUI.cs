fb6372ae9f7f2280d108a6b83d3257eaf5160c48
Arman Akhmetov <eon704@gmail.com>
2025-03-15 00:07:34 -0400

    Disabled sprites for state buttons logic

--- Assets/Scripts/UI/StateCardUI.cs
+++ Assets/Scripts/UI/StateCardUI.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -6,7 +7,11 @@ using UnityEngine.UI;
 
 public class StateCardUI : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
+    [SerializeField] private Player.StateType stateType;
+    [SerializeField] private TMP_Text movesText;
+    [SerializeField] private Player player;
     [SerializeField] private GameObject cardGlow; 
+    
     private Button _button;
 
     private void Awake()
@@ -17,6 +22,7 @@ public class StateCardUI : MonoBehaviour, ISelectHandler, IDeselectHandler
     private void Start()
     {
         cardGlow.SetActive(false);
+        player.OnMovesLeftChanged.AddListener(this.OnMove);
     }
 
     public void OnSelect(BaseEventData eventData)
@@ -30,5 +36,14 @@ public class StateCardUI : MonoBehaviour, ISelectHandler, IDeselectHandler
     public void OnDeselect(BaseEventData eventData)
     {
         cardGlow.SetActive(false);
+    }
+    
+    private void OnMove(Player.StateType moveStateType, int transformationsLeft)
+    {
+        if (moveStateType != this.stateType) 
+            return;
+        
+        movesText.text = transformationsLeft.ToString("D2");
+        _button.interactable = transformationsLeft > 0;
     }
 }
--- Assets/Scripts/UI/TransformationText.cs
+++ Assets/Scripts/UI/TransformationText.cs
@@ -8,14 +8,6 @@ public class TransformationText : MonoBehaviour
     [SerializeField] private TMP_Text text;
     [SerializeField] private Player player;
 
-    private void Start()
-    {
-        this.player.OnMovesLeftChanged.AddListener(this.OnTransformation);
-    }
     
-    private void OnTransformation(Player.StateType stateType, int transformationsLeft)
-    {
-        if (stateType != this.stateType) return;
-        this.text.text = transformationsLeft.ToString("D2");
-    }
+    
 }
