775e2a7603063351916dd543687593997c231a2c
Arman Akhmetov <eon704@gmail.com>
2025-04-12 02:37:17 -0400

    Chinese font

--- Assets/Scripts/Localization/LocaleSelector.cs
+++ Assets/Scripts/Localization/LocaleSelector.cs
@@ -1,22 +1,11 @@
-using System;
 using UnityEngine;
-using UnityEngine.Localization.Settings;
 using UnityEngine.UI;
-using System.Collections;
 
 namespace Localization
 {
     public class LocaleSelector : MonoBehaviour
     {
-        private enum Locale
-        {
-            English,
-            Russian,
-            Kazakh,
-            Chinese
-        }
-
-        [SerializeField] private Locale locale;
+        [SerializeField] private LocalizationManager.Locale locale;
         private Button _button;
 
         private void Start()
@@ -27,37 +16,7 @@ namespace Localization
 
         private void SetLocale()
         {
-            StartCoroutine(ChangeLocaleCoroutine());
-        }
-
-        private IEnumerator ChangeLocaleCoroutine()
-        {
-            _button.interactable = false;
-
-            var localeIndex = locale switch
-            {
-                Locale.English => 0,
-                Locale.Russian => 1,
-                Locale.Kazakh => 2,
-                Locale.Chinese => 3,
-                _ => 0
-            };
-
-            var selectedLocale = LocalizationSettings.AvailableLocales.Locales[localeIndex];
-            var operation = LocalizationSettings.InitializationOperation;
-            yield return operation;
-
-            if (operation.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
-            {
-                LocalizationSettings.SelectedLocale = selectedLocale;
-                PlayerPrefs.SetInt("localeIndex", localeIndex);
-            }
-            else
-            {
-                Debug.LogError("Failed to initialize localization.");
-            }
-
-            _button.interactable = true;
+            LocalizationManager.Instance.ChangeLocale(locale);
         }
     }
 }  
---
+++ Assets/Scripts/Managers/LocalizationManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LocalizationManager : Singleton<LocalizationManager>
+{
+  public enum Locale
+  {
+    English,
+    Russian,
+    Kazakh,
+    Chinese
+  }
+
+  private IEnumerator Start()
+  {
+    yield return LocalizationSettings.InitializationOperation;
+
+    var localeIndex = PlayerPrefs.GetInt("localeIndex", -1);
+    ChangeLocale(localeIndex);
+  }
+
+  public void ChangeLocale(Locale locale)
+  {
+    var localeIndex = locale switch
+    {
+      Locale.English => 0,
+      Locale.Russian => 1,
+      Locale.Kazakh => 2,
+      Locale.Chinese => 3,
+      _ => 0
+    };
+    ChangeLocale(localeIndex);
+  }
+
+  private void ChangeLocale(int localeIndex)
+  {
+    var selectedLocale = LocalizationSettings.AvailableLocales.Locales[localeIndex];
+    LocalizationSettings.SelectedLocale = selectedLocale;
+    PlayerPrefs.SetInt("localeIndex", localeIndex);
+  }
+}
