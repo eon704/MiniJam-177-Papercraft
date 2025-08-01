09168599f606cbc27b29456377ec358119b938a7
Arman Akhmetov <eon704@gmail.com>
2025-08-01 12:07:06 +0500

    Updated settings for AdManager

--- Assets/Scripts/Managers/AdManager.cs
+++ Assets/Scripts/Managers/AdManager.cs
@@ -5,34 +5,12 @@ using Unity.Services.Analytics;
 
 public class AdManager : Singleton<AdManager>
 {
-    private const string AppKey = "223b9d07d";
-
     protected override void Awake()
     {
         base.Awake();
         
         LevelPlay.OnInitSuccess += OnInitSuccess;
         LevelPlay.OnInitFailed += OnInitFailed;
-        
-        // Initialize LevelPlay with User ID from Unity Gaming Services
-        InitializeLevelPlayWithUserId();
-    }
-
-    private async void InitializeLevelPlayWithUserId()
-    {
-        // Wait for Unity Gaming Services to be initialized first
-        while (!UnityServices.State.Equals(ServicesInitializationState.Initialized))
-        {
-            await System.Threading.Tasks.Task.Yield();
-        }
-        
-        // Get the Unity User ID from Analytics service
-        string userId = AnalyticsService.Instance.GetAnalyticsUserID();
-        
-        // Initialize LevelPlay with the User ID
-        LevelPlay.Init(AppKey, userId);
-        
-        Debug.Log($"LevelPlay initialized with User ID: {userId}");
     }
 
     private void OnInitSuccess(LevelPlayConfiguration configuration)
